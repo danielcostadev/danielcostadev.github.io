b2df1f426e6cac81f98323116cd0dc67c688adf5
danielcostadev <danielcostamarketing@gmail.com>
2024-06-09 14:57:09 -0300

    Implementando Google Forms novo metódo

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSc_W8CdeJTu2NYstnI6aIq677qlUlMTak9Nwplhi-iMzviuNw/viewform?usp=sf_link",
+                url: "https://docs.google.com/forms/d/e/1FAIpQLSc_W8CdeJTu2NYstnI6aIq677qlUlMTak9Nwplhi-iMzviuNw/formResponse",
                 type: "POST",
                 data: {
                     "entry.517041826": name,
