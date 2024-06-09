3a87dba570262796369fee0e4ca4a7a696776bc4
danielcostadev <danielcostamarketing@gmail.com>
2024-06-09 16:29:36 -0300

    Alteração em caminho de script

--- mail/contact.js
+++ mail/contact.js
@@ -16,7 +16,7 @@ $(function () {
 
             $.ajax({
                 url: "contact.php",
-                type: "POST",        
+                type: "POST",
                 data: {
                     name: name,
                     email: email,
