453ea3a16586a9c9e1fad8b517fa564d3f8384f5
danielcostadev <danielcostamarketing@gmail.com>
2024-06-09 19:22:13 -0300

    update

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "mail/contact.php",
+                url: "https://formsubmit.co/danielcostamarketing@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
@@ -29,7 +29,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Sua mensagem foi enviada. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
@@ -38,7 +38,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Desculpe " + name + ", parece que nosso servidor de e-mails não está respondendo. Por favor, tente novamente mais tarde!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
