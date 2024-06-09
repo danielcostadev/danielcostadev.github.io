3f823b73306681c613eeaec90edad3df23c86450
danielcostadev <danielcostamarketing@gmail.com>
2024-06-09 12:53:08 -0300

    Adcionando camada de segurança ao formulário

--- mail/contact.php
+++ mail/contact.php
@@ -1,6 +1,14 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
+function isInjected($str) {
+  $injections = array('(\n+)', '(\r+)', '(\t+)', '(%0A+)', '(%0D+)', '(%08+)', '(%09+)');
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  return preg_match($inject, $str);
+}
+
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) || isInjected($_POST['name']) || isInjected($_POST['email']) || isInjected($_POST['subject'])) {
+  http_response_code(400);
+  echo 'Por favor, preencha todos os campos corretamente e forneça um e-mail válido.';
   exit();
 }
 
@@ -10,11 +18,13 @@ $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "danielcostadev@outlook.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "NOVA MENSAGEM DO PORTIFOLIO.\n\n"."Aqui os detalhes:\n\nNome: $name\n\n\nE-mail: $email\n\nAssunto: $m_subject\n\nMensagem: $message";
-$header = "De: $email";
-$header .= "Responda para: $email";	
+$subject = "$m_subject: $name";
+$body = "NOVA MENSAGEM DO PORTIFOLIO.\n\nAqui os detalhes:\n\nNome: $name\n\nE-mail: $email\n\nAssunto: $m_subject\n\nMensagem: $message";
+$header = "De: $email\r\n";
+$header .= "Reply-To: $email\r\n";
 
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $subject, $body, $header)) {
   http_response_code(500);
+  echo 'Ocorreu um erro ao enviar sua mensagem. Tente novamente mais tarde.';
+}
 ?>
